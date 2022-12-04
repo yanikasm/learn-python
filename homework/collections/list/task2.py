f439b9ef18025f660eff0b28dc859ff7c21de561
yanikasm <yanikasm@icloud.com>
2022-12-04 18:38:14 +0300

    solved again!

--- homework/collections/list/task2.py
+++ homework/collections/list/task2.py
@@ -6,13 +6,10 @@ nums = list(map(int, input().split(",")))
 ans = []
 
 for i in range(0, len(nums)):
-   ans.append(nums[nums[i]])
+    ans.append(nums[nums[i]])
 print(ans)
 
-
-
-
-#nums = [0, 2, 1, 5, 3, 4]
+# nums = [0, 2, 1, 5, 3, 4]
 # i = 5
 # x = nums[i] = 4
 # y = nums[x]
